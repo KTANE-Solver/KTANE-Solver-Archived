69a0c035181e5509b7a54f6069d4022d7c202a76
Nya Bentley <nyabentley@gmail.com>
2021-02-27 13:14:50 -0500

    Add project files.

---
+++ EdgeworkSelectionForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KTANE_Solver
+{
+    //Author: Nya Bentley
+    //Date: 2/27/21
+    //Purpose: Used to prompt the user if they want to get 
+    //         the edgework from the Edgework.txt or type 
+    //         it in manually
+            
+    public partial class EdgeworkSelectionForm : Form
+    {
+        public EdgeworkSelectionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
