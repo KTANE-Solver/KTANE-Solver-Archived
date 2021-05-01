e7110db06ce6cc5a6ccf632ba5931cde8385c730
Nya <nyabentley@gmail.com>
2021-04-30 20:58:07 -0400

    wrote out comments for the "Chess" module

---
+++ Chess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KTANE_Solver
+{
+    //Date: 4/30/21
+    //Purpose: solves the "chess" module 
+    class Chess
+    {
+        //FIELDS
+
+        //a 6x6 2d char array that represents the board
+
+        //the character for piece 1
+
+        //the character for piece 2
+
+        //the character for piece 3
+
+        //the character for piece 4
+
+        //the character for piece 5
+
+        //the character for piece 6
+
+        //CONSTRUCTOR
+
+        //will take 6 characters for the pieces, the bomb, and the streamwriter
+
+
+        //METHODS
+
+        //a method that places all the pieces
+
+        //a method that will tell where a king will go
+
+        //a method that will tell where a rook will go
+
+        //a method that will tell where a knight will go
+
+        //a method that will tell where a bishop will go
+
+        //a method that will tell where a queen will go
+
+        //a method that will tell if a tile on a board is black or white
+
+        //a method that will tell the answer (solve)
+
+    }
+}
