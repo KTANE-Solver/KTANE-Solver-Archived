2006039476d78ab5b9114be945f7cd78f48397ed
Nya Bentley <nyabentley@gmail.com>
2021-10-02 15:04:04 -0400

    Added a feature where program won't start if the following files are missing:
    edgework.txt
    logfile.txt
    keypad photos
    maze arrows

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace KTANE_Solver
 {
@@ -16,7 +17,90 @@ namespace KTANE_Solver
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            //check to make sure no files are missing
+
+            //edgework
+
+            //logfile
+
+            //keypad folder
+
+            //maze arrow folder and all its images
+
+            if (!FileExists("Edgework.txt") || !FileExists("LogFile.txt") || !DirectoryExists("Keypad Pictures") || !DirectoryExists("Maze Arrows"))
+            {
+                return;
+            }
+
+            //all keypad pictures
+            if (!KeypadFileExist("3") || !KeypadFileExist("6") || !KeypadFileExist("A") ||
+                !KeypadFileExist("AE") || !KeypadFileExist("B") || !KeypadFileExist("Backwards C") ||
+                !KeypadFileExist("Black Star") || !KeypadFileExist("Butt") || !KeypadFileExist("C") ||
+                !KeypadFileExist("Copyright") || !KeypadFileExist("E") || !KeypadFileExist("H") ||
+                !KeypadFileExist("Hashtag") || !KeypadFileExist("Lambda") || !KeypadFileExist("Lightning") ||
+                !KeypadFileExist("N") || !KeypadFileExist("O") || !KeypadFileExist("Omega") ||
+                !KeypadFileExist("Paragraph") || !KeypadFileExist("Question Mark") || !KeypadFileExist("Smily Face") ||
+                !KeypadFileExist("Squid") || !KeypadFileExist("Swirl") || !KeypadFileExist("Swirl") ||
+                !KeypadFileExist("Unfinished R") || !KeypadFileExist("White Star") || !KeypadFileExist("X"))
+            {
+                return;
+            }
+
+            //all maze arrows
+            if (!MazeFileExists("up arrow") || !MazeFileExists("up arrow") || !MazeFileExists("up arrow") || !MazeFileExists("up arrow"))
+            {
+                return;
+            }
+
+            
             Application.Run(new EdgeworkSelectionForm());
         }
+
+        static bool FileExists(String name)
+        {
+            if (!File.Exists(name))
+            { 
+                MessageBox.Show($"Unable to find {name}. Redownload bot and try again", "File Missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool DirectoryExists(String name)
+        {
+            if (!Directory.Exists(name))
+            {
+                MessageBox.Show($"Unable to find {name}. Redownload bot and try again", "Directory Missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool KeypadFileExist(String name)
+        {
+            if (!File.Exists($"Keypad Pictures/{name}.png"))
+            {
+                MessageBox.Show($"Unable to find {name}.png. Redownload bot and try again", "File Missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool MazeFileExists(string name)
+        { 
+            if(!File.Exists($"Maze Arrows/{name}.png"))
+            {
+                MessageBox.Show($"Unable to find {name}.png. Redownload bot and try again", "File Missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
     }
+    
 }
+
