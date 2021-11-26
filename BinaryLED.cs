33fa38259f857008bcc6ec3f4598411e406d41a8
Nya Bentley <nyabentley@gmail.com>
2021-11-26 14:25:54 -0500

    added pseudo code for BinaryLED

---
+++ BinaryLED.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTANE_Solver
+{
+    class BinaryLED
+    {
+        //METHOD
+        /*
+         Solve
+         -Given a sequence (size will depending on preference), figure out when user can cut red, green, or blue wire
+         -Answer will mostly likey be given with colored blocks or number (depending on preferences)
+         */
+    }
+}
