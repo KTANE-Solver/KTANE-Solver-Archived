3d156ae7eb62eb75448b3614912c1a6e9d0b7c24
Nya Bentley <nyabentley@gmail.com>
2021-11-26 14:41:57 -0500

    Added pseudo code for Bitmaps

---
+++ Bitmaps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTANE_Solver
+{
+    class Bitmaps
+    {
+        //FEILDS
+        /*
+        -2d array that represetns entire grid
+        -2d array that represetns 1st quadrant
+        -2d array that represetns 2nd quadrant
+        -2d array that represetns 3rd quadrant
+        -2d array that represetns 4th quadrant
+        */
+
+        //METHODS
+        /*
+         Find first correct rule
+
+         find and module answer if needed
+        */
+        /*
+        -fills 1st, 2nd, 3rd, and 4th quadrants
+        */
+
+    }
+}
