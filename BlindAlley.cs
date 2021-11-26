cd35313ee253b48f60e6aed2e05a2b73b073b637
Nya Bentley <nyabentley@gmail.com>
2021-11-26 15:05:38 -0500

    Added pseudo code for Blind Alley

---
+++ BlindAlley.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTANE_Solver
+{
+    class BlindAlley
+    {
+        //FIELDS
+        /*
+        -(Section) Top left
+        -(Section) Top mid
+        -(Section) Mid left
+        -(Section) Mid
+        -(Section) Middle Right
+        -(Section) Bottom Left
+        -(Section) Bottom Mid
+        -(Section) Bottm Right
+        */
+
+        //METHOD
+        /*
+         Find which sections have the highest true conditions
+
+         Return those sections
+         */
+
+        //CLASSES
+        /*
+        Class that will hold 4 conditions called section
+        -Paramaters: 4 conditons, number of conditions that are true
+        */
+    }
+}
