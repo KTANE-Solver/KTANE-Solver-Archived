310f67f311c57c9b4dd9c192a79dfa416230da5d
Nya Bentley <nyabentley@gmail.com>
2021-11-26 14:17:11 -0500

    Added pseudo code for Battleship

---
+++ Battleship.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTANE_Solver
+{
+    class Battleship
+    {
+        //FIELDS
+        /*
+        -2d array that'll represent board
+        -1 sized ship num
+        -2 sized ship num
+        -3 sized ship num
+        -4 sized ship num
+        */
+
+        //METHODS
+        /*
+        Fill Row/Column
+        -If there are 0 in a row/column, fill that with water
+         */
+
+        /*
+        Guaruntee n spaces
+        -If there is 1 n sized ship, and it can fit in one posible row/colun,
+         enter middle spaces as hits where area overlap
+         */
+
+        /*
+        Complete ship
+        -If it is gauranteed that a ship is built, surround adjacent area with water
+         */
+
+
+    }
+}
