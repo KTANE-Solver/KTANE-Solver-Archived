57afc9628977f647d620b9bb11cecb40da444d97
Nya Bentley <nyabentley@gmail.com>
2021-11-26 18:35:14 -0500

    Added a little more pesudeo code to Battleship

--- Battleship.cs
+++ Battleship.cs
@@ -18,15 +18,55 @@ namespace KTANE_Solver
         */
 
         //METHODS
+
         /*
-        Fill Row/Column
-        -If there are 0 in a row/column, fill that with water
+        Tells if the puzzle is valid
+
+        -The number of 1 sized ships is the same or less than what the bombs offers
+        -The number of 2 sized ships is the same or less than what the bombs offers
+        -The number of 3 sized ships is the same or less than what the bombs offers
+        -The number of 4 sized ships is the same or less than what the bombs offers
+        -The number of hits in each row is the same or less than what the bombs offers
+        -The number of hits in each column is the same or less than what the bombs offers
+        */
+
+        /*
+        Find the safe locations
+        -Make a list of letters, and rows
+        -Mark the safe locations
+        -Use these lists until either run out of elements
+        -Use port num, indicator num, and battery num to find last safe location
+
+
+        */
+
+        /*
+        */
+
+        /*
+        Fill Water Row/Column
+        -If there are 0 in a row/column (or all the hits are filled), fill that with water
+         */
+
+        /*
+        Fill Hit Row/Column
+        -If number of emtpy spaces in a row/column match the number hits, fill that with hits
          */
 
         /*
         Guaruntee n spaces
         -If there is 1 n sized ship, and it can fit in one posible row/colun,
          enter middle spaces as hits where area overlap
+         */
+
+        /*
+        Eliminating squares
+        -If there are no 1 sized ships that haven't been placed yet, and there are single spaces that are surrounded by water is all directions, mark that as water
+
+        -If there are no 2 sized ships that haven't been placed yet, and there are two spaces that are surrounded by water is all directions, mark that as water
+
+        -If there are no 3 sized ships that haven't been placed yet, and there are three spaces that are surrounded by water is all directions, mark that as water
+
          */
 
         /*
