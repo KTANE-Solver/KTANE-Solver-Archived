0f5be0b42e2b58a0b79eaaf4dc25113fed03cf24
Nya Bentley <nyabentley@gmail.com>
2021-11-26 13:20:38 -0500

    Got rid of 3D Maze

--- ModuleSelectionForm.cs
+++ ModuleSelectionForm.cs
@@ -34,7 +34,6 @@ namespace KTANE_Solver
         StreamWriter logFileWriter;
 
         //module forms
-        private _3DMazeForm threeDMazeForm;
         private AdjacentLettersForm adjacentLettersForm;
         private AdventureGameForm adventureGameForm;
         private BinaryPuzzleForm binaryForm;
@@ -128,8 +127,8 @@ namespace KTANE_Solver
             moduleComboBox.Items.Clear();
 
 
-            String[] modules = new String[] {"3D Maze", "Adjacent Letters", "Adventure Game", "Binary Puzzle", "Bulb", "Button", "Cheap Checkout", "Chess", "Color Math", "Complicated Wires", "FizzBuzz", "Gamepad", "Ice Cream", "Keypad", "Light Cycle", "Logic", "Maze", "Memory", "Monsplode Trading Cards", "Morse Code", "Murder","Number Pad", "Password", "Poker", "Rock Paper Scissors Lizard Spock", "Rubik's Cube", "Silly Slots", "Simon Says", "Switches", "Two Bits", "Who's on First", "Wires", "Wires Sequence", "Word Search" };
-            //String[] modules = new String[] { "Binary Puzzle", "Cheap Checkout", "Chess", "Complicated Wires", "Ice Cream", "Keypad", "Logic", "Maze", "Murder", "Poker", "Silly Slots", "Who's on First", "Wires"};
+            //String[] modules = new String[] {"Adjacent Letters", "Adventure Game", "Binary Puzzle", "Bulb", "Button", "Cheap Checkout", "Chess", "Color Math", "Complicated Wires", "FizzBuzz", "Gamepad", "Ice Cream", "Keypad", "Light Cycle", "Logic", "Maze", "Memory", "Monsplode Trading Cards", "Morse Code", "Murder","Number Pad", "Password", "Poker", "Rock Paper Scissors Lizard Spock", "Rubik's Cube", "Silly Slots", "Simon Says", "Switches", "Two Bits", "Who's on First", "Wires", "Wires Sequence", "Word Search" };
+            String[] modules = new String[] { "Binary Puzzle", "Cheap Checkout", "Chess", "Complicated Wires", "Ice Cream", "Keypad", "Logic", "Maze", "Murder", "Poker", "Silly Slots", "Who's on First", "Wires"};
 
             moduleComboBox.Items.AddRange(modules);
             moduleComboBox.Text = modules[0];
@@ -269,21 +268,6 @@ namespace KTANE_Solver
 
             switch (module)
             {
-                case "3D Maze":
-                    if (threeDMazeForm == null)
-                    {
-                        threeDMazeForm = new _3DMazeForm(bomb, logFileWriter, this);
-                    }
-
-                    else
-                    {
-                        threeDMazeForm.UpdateForm(bomb, logFileWriter, this);
-                    }
-
-                    threeDMazeForm.Show();
-
-                    break;
-
                 case "Adjacent Letters":
                     if (adjacentLettersForm == null)
                     {
