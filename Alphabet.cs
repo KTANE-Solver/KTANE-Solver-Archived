1240e44609e94151899e4fc987efb50158096f1f
Nya Bentley <nyabentley@gmail.com>
2021-11-26 13:53:03 -0500

    Added pseudo code for aphabet

---
+++ Alphabet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTANE_Solver
+{
+    class Alphabet
+    {
+        //FIELDS
+        /*
+         String that'll hold answer
+         
+         */
+
+        //METHODS
+
+        /*
+          Solve method
+          -Find longest word
+          -append word in asnwer
+          -repeat with letters that weren't used
+          -if no words are found, put remain letters in alphabetical order and append in answer
+          -return answer
+         */
+
+        /*
+         A method that finds a word
+         -Parameters: 4 letters
+         -Return word (empty str if none found)         
+         */
+
+        /*
+         A method that finds a word
+         -Parameters: 3 letters
+         -Return word (empty str if none found)         
+         */
+
+        /*
+         A method that finds a word
+         -Parameters: 2 letters
+         -Return word (empty str if none found)         
+         */
+    }
+}
