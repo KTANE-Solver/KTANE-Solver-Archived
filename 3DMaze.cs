4a883c89743cf1dc78100d6c469d296d619e39ce
Nya Bentley <nyabentley@gmail.com>
2021-11-26 13:39:35 -0500

    Added a llittle bit of pseudo code for 3D Maze

---
+++ 3DMaze.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTANE_Solver
+{
+    class _3DMaze
+    {
+        //FIELDS
+        /* a 2D Node array that will represent the maze
+         */
+
+        //METHODS
+        /* Method that will fill in the 2d array
+           -Parameters (the three letters in the maze)
+
+           Method that will find find where the user needs to go (Dijkstra)
+           -Paramaters (starting position (and where user is facing), ending position)
+           -Returns a list of directions
+           
+         */
+
+        //CLASSES
+        /* Node class that contains the following fields:
+         -Row (int)
+         -Column (int)
+         -Charater (char)
+         -North neighbor (Node)
+         -East neighbor (Node)
+         -South neighbor (Node)
+         -West neighbor (Node)
+        */
+    }
+}
