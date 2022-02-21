4a5e8646190f23a56777a129dec8b25ba456375c
Nya <nyabentley@gmail.com>
2022-02-21 12:22:48 -0500

    Created header for Light Cycle

--- LightCycleForm.cs
+++ LightCycleForm.cs
@@ -132,6 +132,8 @@ namespace KTANE_Solver
                     cycle += "Y";
                 }
 
+            PrintHeader("Light Cycle");
+
             new LightCycle(Bomb, LogFileWriter, cycle);
             UpdateForm(Bomb, LogFileWriter, ModuleSelectionForm);
         }
