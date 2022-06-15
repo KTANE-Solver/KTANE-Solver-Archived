7f0885e2a8f751cd9a5ddb3da2f584f461d0f5d4
Nya <nyabentley@gmail.com>
2022-06-15 19:21:44 -0400

    Started working on Colored Squares Unit Tests

---
+++ ModuleTests/Colored Squares.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using KTANE_Solver;
+using System.IO;
+namespace ModuleTests
+{
+    [TestClass]
+    public class ColoredSquares
+    {
+        Bomb bomb;
+        StreamWriter streamWriter;
+        KTANE_Solver.ColoredSquares.Color[,] grid;
+    }
+}
