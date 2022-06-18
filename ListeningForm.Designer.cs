8f136036d38407de1846212e9d5e67b5c9380f7b
Nya <nyabentley@gmail.com>
2022-06-17 20:29:45 -0400

    Finished Listening module along with debug lines

---
+++ ListeningForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace KTANE_Solver
+{
+    partial class ListeningForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backButton = new System.Windows.Forms.Button();
+            this.strikeButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.soundComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 84);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(97, 35);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // strikeButton
+            // 
+            this.strikeButton.Location = new System.Drawing.Point(140, 84);
+            this.strikeButton.Name = "strikeButton";
+            this.strikeButton.Size = new System.Drawing.Size(97, 35);
+            this.strikeButton.TabIndex = 1;
+            this.strikeButton.Text = "Strike";
+            this.strikeButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(276, 84);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(97, 35);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // soundComboBox
+            // 
+            this.soundComboBox.FormattingEnabled = true;
+            this.soundComboBox.Location = new System.Drawing.Point(129, 38);
+            this.soundComboBox.Name = "soundComboBox";
+            this.soundComboBox.Size = new System.Drawing.Size(121, 21);
+            this.soundComboBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sound:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 131);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.soundComboBox);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.strikeButton);
+            this.Controls.Add(this.backButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button strikeButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.ComboBox soundComboBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
