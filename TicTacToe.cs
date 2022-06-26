0a1131096a274a831af74c8d5b7c0e565ceab9b1
Nya <nyabentley@gmail.com>
2022-06-26 15:54:26 -0400

    Created Tic Tac Toe

---
+++ TicTacToe.cs
@@ -0,0 +1,507 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace KTANE_Solver
+{
+    public class TicTacToe : Module
+    {
+        private char[,] grid;
+        private int currentRow;
+        public TicTacToe(Bomb bomb, StreamWriter logFileWriter, char[,] grid) : base(bomb, logFileWriter, "Tic Tac Toe")
+        { 
+            this.grid = grid;
+        }
+
+        public void FindStartingRow()
+        {
+            List<int> row = new List<int>();
+
+            if (Bomb.LastDigit % 2 == 0)
+            {
+                row.AddRange(new int[] { 5, 6, 7, 8, 9 });
+            }
+
+            else
+            { 
+                row.AddRange(new int[] { 1, 2, 3, 4});
+
+            }
+
+            if (Bomb.Parallel.Visible)
+            {
+                for (int i = row.Count - 1; i < -1; i--)
+                {
+                    if (row[i] % 2 == 1)
+                    {
+                        row.RemoveAt(i);
+                    }
+                }
+            }
+
+            else
+            {
+                for (int i = row.Count - 1; i < -1; i--)
+                {
+                    if (row[i] % 2 == 0)
+                    {
+                        row.RemoveAt(i);
+                    }
+                }
+            }
+
+            if (Bomb.UnlitIndicatorsList.Count > Bomb.LitIndicatorsList.Count)
+            {
+                if (row[0] < row[1])
+                {
+                    currentRow = row[0];
+                }
+
+                else
+                {
+                    currentRow = row[1];
+                }
+            }
+
+            else if (Bomb.LitIndicatorsList.Count < Bomb.UnlitIndicatorsList.Count)
+            {
+                if (row[0] > row[1])
+                {
+                    currentRow = row[0];
+                }
+
+                else
+                {
+                    currentRow = row[1];
+                }
+            }
+
+            else
+            {
+                currentRow = (row[0] + row[1]) / 2;
+            }
+        }
+
+        public int[] Solve(char symbol)
+        {
+            char targetSymbol = PlaceSymbol(symbol);
+
+            int[] targetCoordinates = NumberIsInGrid(targetSymbol);
+
+            while (targetCoordinates.Length == 1)
+            {
+                currentRow++;
+                targetCoordinates = NumberIsInGrid(targetSymbol);
+            }
+
+            if (!MakesTicTacToe(targetCoordinates[0], targetCoordinates[1]))
+            {
+                return new int[] { targetCoordinates[0], targetCoordinates[1], symbol };
+            }
+
+            else
+            {
+                symbol = GetOppositeSymbol(symbol);
+
+                targetSymbol = PlaceSymbol(symbol);
+
+                targetCoordinates = NumberIsInGrid(targetSymbol);
+
+                while (targetCoordinates.Length == 1)
+                {
+                    currentRow++;
+                    targetCoordinates = NumberIsInGrid(targetSymbol);
+                }
+
+                if (!MakesTicTacToe(targetCoordinates[0], targetCoordinates[1]))
+                {
+                    ShowAnswer("Pass", false);
+                    return new int[] { targetCoordinates[0], targetCoordinates[1], symbol };
+                }
+
+                else
+                {
+                    ShowAnswer("Double Pass", false);
+                    return new int[] { -1 };
+                }
+            }
+        }
+
+        private char GetOppositeSymbol(char symbol)
+        {
+            if (symbol == 'X')
+            {
+                return 'O';
+            }
+
+            return 'X';
+        }
+
+        private char PlaceSymbol(char symbol)
+        {
+            int xCount = XCount();
+            int oCount = OCount();
+
+            if (xCount > oCount)
+            {
+                if (symbol == 'X')
+                {
+                    switch (currentRow)
+                    {
+                        case 1:
+                            return '9';
+
+                        case 2:
+                            return '5';
+
+
+                        case 3:
+                            return '7';
+
+
+                        case 4:
+                            return '4';
+
+
+                        case 5:
+                            return '9';
+
+
+                        case 6:
+                            return '8';
+
+                        case 7:
+                            return '6';
+
+                        case 8:
+                            return '2';
+
+                        default:
+                            return '3';
+                    }
+                }
+
+                else
+                {
+                    switch (currentRow)
+                    {
+                        case 1:
+                            return '3';
+
+                        case 2:
+                            return '6';
+
+
+                        case 3:
+                            return '8';
+
+
+                        case 4:
+                            return '5';
+
+
+                        case 5:
+                            return '4';
+
+
+                        case 6:
+                            return '7';
+
+                        case 7:
+                            return '1';
+
+                        case 8:
+                            return '2';
+
+                        default:
+                            return '9';
+                    }
+                }
+            }
+
+
+            else if (xCount > oCount)
+            {
+                if (symbol == 'X')
+                {
+                    switch (currentRow)
+                    {
+                        case 1:
+                            return '3';
+
+                        case 2:
+                            return '6';
+
+
+                        case 3:
+                            return '2';
+
+
+                        case 4:
+                            return '7';
+
+
+                        case 5:
+                            return '1';
+
+
+                        case 6:
+                            return '5';
+
+                        case 7:
+                            return '8';
+
+                        case 8:
+                            return '9';
+
+                        default:
+                            return '4';
+                    }
+                }
+
+                else
+                {
+                    switch (currentRow)
+                    {
+                        case 1:
+                            return '9';
+
+                        case 2:
+                            return '7';
+
+
+                        case 3:
+                            return '1';
+
+
+                        case 4:
+                            return '8';
+
+
+                        case 5:
+                            return '6';
+
+
+                        case 6:
+                            return '2';
+
+                        case 7:
+                            return '4';
+
+                        case 8:
+                            return '5';
+
+                        default:
+                            return '3';
+                    }
+                }
+            }
+
+            else
+            {
+
+                {
+                    if (symbol == 'X')
+                    {
+                        switch (currentRow)
+                        {
+                            case 1:
+                                return '8';
+
+                            case 2:
+                                return '1';
+
+
+                            case 3:
+                                return '5';
+
+
+                            case 4:
+                                return '9';
+
+
+                            case 5:
+                                return '7';
+
+
+                            case 6:
+                                return '4';
+
+                            case 7:
+                                return '3';
+
+                            case 8:
+                                return '2';
+
+                            default:
+                                return '6';
+                        }
+                    }
+
+                    else
+                    {
+                        switch (currentRow)
+                        {
+                            case 1:
+                                return '1';
+
+                            case 2:
+                                return '2';
+
+
+                            case 3:
+                                return '8';
+
+
+                            case 4:
+                                return '6';
+
+
+                            case 5:
+                                return '3';
+
+
+                            case 6:
+                                return '4';
+
+                            case 7:
+                                return '9';
+
+                            case 8:
+                                return '5';
+
+                            default:
+                                return '7';
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private int XCount()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (grid[i, j] == 'X')
+                    {
+                        sum++;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private int OCount()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (grid[i, j] == 'O')
+                    {
+                        sum++;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private int[] NumberIsInGrid(char target)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                { 
+                    if(grid[i,j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return new int[] { -1 };
+        }
+
+        private bool MakesTicTacToe(int rowNum, int colNum)
+        {
+            List<char> row = new List<char>();
+            int xCount;
+            //horizontal
+                row.Clear();
+                for (int j = 0; j < 3; j++)
+                {
+                    row.Add(grid[rowNum, j]);
+                }
+
+                xCount = row.Where(x => x.Equals('X')).Count();
+
+                if (xCount == 0 || xCount == 3)
+                {
+                    return true;
+                }
+
+            //vertical
+                row.Clear();
+                for (int j = 0; j < 3; j++)
+                {
+                    row.Add(grid[j, colNum]);
+                }
+
+                xCount = row.Where(x => x.Equals('X')).Count();
+
+                if (xCount == 0 || xCount == 3)
+                {
+                    return true;
+                }
+
+            //diagnoal
+
+            row.Clear();
+
+            if(rowNum == colNum)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    row.Add(grid[i, i]);
+                }
+
+                xCount = row.Where(x => x.Equals('X')).Count();
+
+                if (xCount == 0 || xCount == 3)
+                {
+                    return true;
+                }
+            }
+
+            row.Clear();
+
+            if (rowNum + colNum == 2)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    row.Add(grid[i, 2 - i]);
+                }
+
+                xCount = row.Where(x => x.Equals('X')).Count();
+
+                if (xCount == 0 || xCount == 3)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TicTacToeForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace KTANE_Solver
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topRowTextBox = new System.Windows.Forms.TextBox();
+            this.bottomRowTextBox = new System.Windows.Forms.TextBox();
+            this.middleRowTextBox = new System.Windows.Forms.TextBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.strikeButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // topRowTextBox
+            // 
+            this.topRowTextBox.Location = new System.Drawing.Point(110, 25);
+            this.topRowTextBox.Name = "topRowTextBox";
+            this.topRowTextBox.Size = new System.Drawing.Size(100, 20);
+            this.topRowTextBox.TabIndex = 0;
+            // 
+            // bottomRowTextBox
+            // 
+            this.bottomRowTextBox.Location = new System.Drawing.Point(110, 110);
+            this.bottomRowTextBox.Name = "bottomRowTextBox";
+            this.bottomRowTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bottomRowTextBox.TabIndex = 1;
+            // 
+            // middleRowTextBox
+            // 
+            this.middleRowTextBox.Location = new System.Drawing.Point(110, 64);
+            this.middleRowTextBox.Name = "middleRowTextBox";
+            this.middleRowTextBox.Size = new System.Drawing.Size(100, 20);
+            this.middleRowTextBox.TabIndex = 2;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 174);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(81, 36);
+            this.backButton.TabIndex = 3;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // strikeButton
+            // 
+            this.strikeButton.Location = new System.Drawing.Point(110, 174);
+            this.strikeButton.Name = "strikeButton";
+            this.strikeButton.Size = new System.Drawing.Size(81, 36);
+            this.strikeButton.TabIndex = 4;
+            this.strikeButton.Text = "Strike";
+            this.strikeButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(200, 174);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(81, 36);
+            this.submitButton.TabIndex = 5;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Top Row:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Middle Row:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Bottom Row:";
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 222);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.strikeButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.middleRowTextBox);
+            this.Controls.Add(this.bottomRowTextBox);
+            this.Controls.Add(this.topRowTextBox);
+            this.Name = "TicTacToeForm";
+            this.Text = "TicTacToeForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox topRowTextBox;
+        private System.Windows.Forms.TextBox bottomRowTextBox;
+        private System.Windows.Forms.TextBox middleRowTextBox;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button strikeButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
