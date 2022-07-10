c536ce1271b7568063a560a4f6eff29cb6f49b60
Nya <nyabentley@gmail.com>
2022-07-10 19:06:54 -0400

    Intplemented Sea Shells module

---
+++ SeaShells.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KTANE_Solver
+{
+    public class SeaShells : Module
+    {
+        private string phrase1;
+        private string phrase2;
+        private string bigButton;
+
+        private string lettters;
+
+        private List<string> answerList;
+
+        public SeaShells(Bomb bomb, StreamWriter logFileWriter, string phrase1, string phrase2, string bigButton) : base(bomb, logFileWriter, "Sea Shells")
+        {
+            this.phrase1 = phrase1;
+            this.phrase2 = phrase2;
+            this.bigButton = bigButton;
+
+            answerList = new List<string>();
+        }
+
+        public void Solve()
+        {
+            string aWord;
+            string bWord;
+            string cWord;
+            string dWord;
+            string eWord;
+
+            switch (bigButton)
+            {
+                case "SHIH TZU":
+                    aWord = "SHOE";
+                    bWord = "SHIH TZU";
+                    cWord = "SHE";
+                    dWord = "SIT";
+                    eWord = "SUSHI";
+                    break;
+
+                case "TOUCAN":
+                    aWord = "CAN";
+                    bWord = "TOUCAN";
+                    cWord = "TUTU";
+                    dWord = "2";
+                    eWord = "CANCAN";
+                    break;
+
+                case "SWITCH":
+                    aWord = "WITCH";
+                    bWord = "SWITCH";
+                    cWord = "ITCH";
+                    dWord = "TWITCH";
+                    eWord = "STITCH";
+                    break;
+
+                default:
+                    aWord = "BURGLAR ALARM";
+                    bWord = "BULGARIA";
+                    cWord = "ARMOUR";
+                    dWord = "BURGER";
+                    eWord = "LLAMA";
+                    break;
+            }
+
+            foreach (char c in lettters)
+            {
+                switch (c)
+                {
+                    case 'A':
+                        answerList.Add(aWord);
+                        break;
+
+                    case 'B':
+                        answerList.Add(bWord);
+                        break;
+
+                    case 'C':
+                        answerList.Add(cWord);
+                        break;
+
+                    case 'D':
+                        answerList.Add(dWord);
+                        break;
+
+                    default:
+                        answerList.Add(eWord);
+                        break;
+                }
+            }
+
+            string answer = string.Join(", ", answerList);
+
+            ShowAnswer(answer, true);
+        }
+
+        private void FindLetters()
+        {
+            switch (phrase1)
+            {
+                case "SHE SELLS":
+            
+                if (phrase2 == "SEA SHELLS")
+                {
+                    lettters = "BDABDAB";
+                }
+
+                else if(phrase2 == "SHE SHELLS")
+                { 
+                    lettters = "ACEEAC";
+                }
+
+                else if (phrase2 == "SEA SELLS")
+                {
+                    lettters = "EACEACE";
+                }
+
+                else
+                {
+                    lettters = "DAABDAB";
+                }
+
+                break;
+
+                case "SHE SHELLS":
+
+                    if (phrase2 == "SEA SHELLS")
+                    {
+                        lettters = "BEEBBE";
+                    }
+
+                    else if (phrase2 == "SHE SHELLS")
+                    {
+                        lettters = "CDCCDB";
+                    }
+
+                    else if (phrase2 == "SEA SELLS")
+                    {
+                        lettters = "EAEAEA";
+                    }
+
+                    else
+                    {
+                        lettters = "BEEDA";
+                    }
+
+                    break;
+
+                default:
+
+                    if (phrase2 == "SEA SHELLS")
+                    {
+                        lettters = "ABABA";
+                    }
+
+                    else if (phrase2 == "SHE SHELLS")
+                    {
+                        lettters = "DBAEC";
+                    }
+
+                    else if (phrase2 == "SEA SELLS")
+                    {
+                        lettters = "EBDADAB";
+                    }
+
+                    else
+                    {
+                        lettters = "CECEC";
+                    }
+
+                    break;
+            }
+        }
+    }
+}
---
+++ SeaShellsForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace KTANE_Solver
+{
+    partial class SeaShellsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Phrase: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(199, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(199, 68);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Second Phrase: ";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(199, 113);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Big Button: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(183, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 36);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Strike";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // SeaShellsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 220);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "SeaShellsForm";
+            this.Text = "SeaShellsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
