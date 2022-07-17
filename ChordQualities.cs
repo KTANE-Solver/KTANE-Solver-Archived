d72db362799fd7f4d5d1a6b579efdcd3b974cfe3
Nya <nyabentley@gmail.com>
2022-07-17 11:21:24 -0400

    Inplemented Chord Qualities

---
+++ ChordQualities.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace KTANE_Solver
+{
+    class ChordQualities : Module
+    {
+        Note note1;
+        Note note2;
+        Note note3;
+        Note note4;
+
+        Note currentRoot;
+        string currentQuality;
+
+        List<string> qualities;
+        
+        Note newRoot;
+        string newQuality;
+
+        public ChordQualities(Bomb bomb, StreamWriter logFileWriter, Note note1, Note note2, Note note3, Note note4) : base(bomb, logFileWriter, "Chord Qualities")
+        {
+            this.note1 = note1;
+            this.note2 = note1;
+            this.note3 = note1;
+            this.note4 = note1;
+
+            RefactorNotes();
+
+            qualities = new List<string>
+            {
+                "X...X..X..X.",
+                "X..X...X..X.",
+                "X...X..X...X",
+                "X..X...X...X",
+                "X..XX.....X.",
+                "X..X..X...X.",
+                "X.X.X..X....",
+                "X.XX...X....",
+                "X...X...X.X.",
+                "X...X...X..X",
+                "X....X.X..X.",
+                "X..X....X..X"
+            };
+
+        }
+
+        public enum Note
+        { 
+            A,
+            ASharp,
+            B,
+            C,
+            CSharp,
+            D,
+            DSharp,
+            E,
+            F,
+            FSharp,
+            G,
+            GSharp
+        }
+
+        public void Solve()
+        {
+            FindQualityAndRoot();
+            FindNewRoot();
+            FindNewQuality();
+            List<Note> answerList = FindAnswer();
+
+            ShowAnswer(string.Join(", ", answerList), true);
+        }
+
+        private void RefactorNotes()
+        {
+            Note[] notes = new Note[] { note1, note2, note3, note4 };
+
+            if (note1 != notes.Min())
+            {
+                int minIndex = Array.IndexOf(notes, notes.Min());
+
+                Note minNote = notes[minIndex];
+                Note firstNote = notes[0];
+
+                notes[0] = minNote;
+                notes[minIndex] = firstNote;
+            }
+
+            if (note4 != notes.Max())
+            {
+                int maxIndex = Array.IndexOf(notes, notes.Max());
+
+                Note minNote = notes[maxIndex];
+                Note lastNote = notes[3];
+
+                notes[3] = minNote;
+                notes[maxIndex] = lastNote;
+            }
+
+            if (note2 > note3)
+            {
+                Note thirdNote = notes[1];
+                Note secondNote = notes[2];
+
+                notes[1] = secondNote;
+                notes[2] = thirdNote;
+            }
+        }
+
+        private void FindQualityAndRoot()
+        {
+            string quality = "";
+            bool correctQuality = false;
+            int startingNote = -1;
+            for(int i = 0; i < 12; i++)
+            {
+                quality = qualities[i];
+
+                for (int j = 1; j > 5; j++)
+                {
+                    startingNote = j;
+                    correctQuality = CoorectQuality(quality, j);
+
+                    if (correctQuality)
+                    {
+                        break;
+                    }
+                }
+
+                if (correctQuality)
+                {
+                    break;
+                }
+            }
+
+            switch (startingNote)
+            {
+                case 1:
+                    currentRoot = note1;
+                    break;
+
+                case 2:
+                    currentRoot = note2;
+                    break;
+
+                case 3:
+                    currentRoot = note3;
+                    break;
+
+                default:
+                    currentRoot = note4;
+                    break;
+            }
+
+            this.currentQuality = quality;
+
+        }
+
+        private int[] FindGaps(int startingNote)
+        {
+            int[] gaps;
+
+            switch (startingNote)
+            {
+                case 1:
+                    gaps = new int[] { note2 - note1, note3 - note2, note4 - note3, note4 - note1};
+                    break;
+
+                case 2:
+                    gaps = new int[] {note3 - note2, note4 - note3, note4 - note1, note2 - note1};
+                    break;
+
+                case 3:
+                    gaps = new int[] {  note4 - note3, note4 - note1, note2 - note1, note3 - note2 };
+                    break;
+
+                default:
+                    gaps = new int[] {  note4 - note1, note2 - note1, note3 - note2, note4 - note3 };
+                    break;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                gaps[i] = Math.Abs(gaps[i]) % 12;
+            }
+
+            return gaps;
+        }
+
+        private int[] FindGaps(string quality)
+        {
+            List<int> gaps = new List<int>();
+
+            List<char> qualityList = quality.ToCharArray().ToList();
+
+
+            while (qualityList.Count != 0)
+            {
+                int counter = 0;
+                char c = qualityList[0];
+
+                if (c != '.')
+                {
+                    qualityList.RemoveAt(0);
+                    continue;
+                }
+
+                while (qualityList.Count != 0 && c == qualityList[0])
+                {
+                    counter++;
+                    qualityList.RemoveAt(0);
+                }
+
+                gaps.Add(counter + 1);
+            }
+
+            return gaps.ToArray();
+        }
+
+        private bool CoorectQuality(string quality, int startingNote)
+        {
+            int[] gaps = FindGaps(startingNote);
+
+            int previousIndex = 0;
+
+            for (int i = 0; i < gaps.Length; i++)
+            {
+                if (i != 3)
+                {
+                    string qualityGaps = quality.Substring(previousIndex, gaps[i]);
+
+                    int dotCount = qualityGaps.Where(x => x == '.').Count();
+
+                    if (qualityGaps[0] != 'X' || quality[qualityGaps.Length - 1] != 'X' || dotCount != qualityGaps.Length - 2)
+                    {
+                        return false;
+                    }
+
+                    previousIndex += gaps[i];
+                }
+
+                else
+                {
+                    string qualityGaps = quality.Substring(previousIndex);
+
+                    if (qualityGaps.Length != gaps[3])
+                    {
+                        return false;
+                    }
+                }
+
+            }
+
+            return true;
+        }
+
+        private void FindNewRoot()
+        {
+            switch (currentQuality)
+            {
+                case "X...X..X..X.":
+                    newRoot = Note.G;
+                    break;
+
+                case "X..X...X..X.":
+                    newRoot = Note.GSharp;
+                    break;
+
+                case "X...X..X...X":
+                    newRoot = Note.ASharp;
+                    break;
+
+                case "X..X...X...X":
+                    newRoot = Note.F;
+                    break;
+
+                case "X..XX.....X.":
+                    newRoot = Note.A;
+                    break;
+
+                case "X..X..X...X.":
+                    newRoot = Note.CSharp;
+                    break;
+
+                case "X.X.X..X....":
+                    newRoot = Note.DSharp;
+                    break;
+
+                case "X.XX...X....":
+                    newRoot = Note.E;
+                    break;
+
+                case "X...X...X.X.":
+                    newRoot = Note.FSharp;
+                    break;
+
+                case "X...X...X..X":
+                    newRoot = Note.C;
+                    break;
+
+                case "X....X.X..X.":
+                    newRoot = Note.D;
+                    break;
+
+                case "X..X....X..X":
+                    newRoot = Note.B;
+                    break;
+            }
+        }
+
+        private void FindNewQuality()
+        {
+            switch (currentRoot)
+            {
+                case Note.A:
+                    newQuality = qualities[11];
+                    break;
+                case Note.ASharp:
+                    newQuality = qualities[9];
+                    break;
+                case Note.B:
+                    newQuality = qualities[1];
+                    break;
+                case Note.C:
+                    newQuality = qualities[5];
+                    break;
+                case Note.CSharp:
+                    newQuality = qualities[7];
+                    break;
+                case Note.D:
+                    newQuality = qualities[2];
+                    break;
+                case Note.DSharp:
+                    newQuality = qualities[4];
+                    break;
+                case Note.E:
+                    newQuality = qualities[10];
+                    break;
+                case Note.F:
+                    newQuality = qualities[6];
+                    break;
+                case Note.FSharp:
+                    newQuality = qualities[0];
+                    break;
+                case Note.G:
+                    newQuality = qualities[3];
+                    break;
+                case Note.GSharp:
+                    newQuality = qualities[8];
+                    break;
+            }
+        }
+
+        private List<Note> FindAnswer()
+        {
+            int [] gaps = FindGaps(newQuality);
+
+            List<Note> answer = new List<Note>();
+
+            answer.Add(newRoot);
+
+
+            Note currentNote = newRoot;
+
+            for (int i = 0; i < gaps.Length; i++)
+            {
+                Note newNote = (Note)((int)(currentNote + gaps[i]) % 12);
+
+                currentNote = newNote;
+
+                answer.Add(currentNote);
+            }
+
+            return answer;
+        }
+
+    }
+}
---
+++ ChordQualitiesForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace KTANE_Solver
+{
+    partial class ChordQualitiesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.note1ComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.note2ComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.note3ComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.note4ComboBox = new System.Windows.Forms.ComboBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.strikeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // note1ComboBox
+            // 
+            this.note1ComboBox.FormattingEnabled = true;
+            this.note1ComboBox.Location = new System.Drawing.Point(132, 12);
+            this.note1ComboBox.Name = "note1ComboBox";
+            this.note1ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.note1ComboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Note 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Note 2:";
+            // 
+            // note2ComboBox
+            // 
+            this.note2ComboBox.FormattingEnabled = true;
+            this.note2ComboBox.Location = new System.Drawing.Point(132, 59);
+            this.note2ComboBox.Name = "note2ComboBox";
+            this.note2ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.note2ComboBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Note 3:";
+            // 
+            // note3ComboBox
+            // 
+            this.note3ComboBox.FormattingEnabled = true;
+            this.note3ComboBox.Location = new System.Drawing.Point(132, 108);
+            this.note3ComboBox.Name = "note3ComboBox";
+            this.note3ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.note3ComboBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Note 4:";
+            // 
+            // note4ComboBox
+            // 
+            this.note4ComboBox.FormattingEnabled = true;
+            this.note4ComboBox.Location = new System.Drawing.Point(132, 155);
+            this.note4ComboBox.Name = "note4ComboBox";
+            this.note4ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.note4ComboBox.TabIndex = 6;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 196);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(79, 33);
+            this.backButton.TabIndex = 8;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(183, 196);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(79, 33);
+            this.submitButton.TabIndex = 9;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // strikeButton
+            // 
+            this.strikeButton.Location = new System.Drawing.Point(97, 196);
+            this.strikeButton.Name = "strikeButton";
+            this.strikeButton.Size = new System.Drawing.Size(79, 33);
+            this.strikeButton.TabIndex = 10;
+            this.strikeButton.Text = "Strike";
+            this.strikeButton.UseVisualStyleBackColor = true;
+            // 
+            // ChordQualitiesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 237);
+            this.Controls.Add(this.strikeButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.note4ComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.note3ComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.note2ComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.note1ComboBox);
+            this.Name = "ChordQualitiesForm";
+            this.Text = "ChordQualitiesForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox note1ComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox note2ComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox note3ComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox note4ComboBox;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button strikeButton;
+    }
+}
