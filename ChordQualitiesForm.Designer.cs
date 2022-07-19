a52842b0381d0fa477cb887b585c8aeb4b2187ec
Nya <nyabentley@gmail.com>
2022-07-18 22:03:35 -0400

    Paased 5 modules with the GUI

--- ChordQualitiesForm.Designer.cs
+++ ChordQualitiesForm.Designer.cs
@@ -45,103 +45,117 @@ namespace KTANE_Solver
             // note1ComboBox
             // 
             this.note1ComboBox.FormattingEnabled = true;
-            this.note1ComboBox.Location = new System.Drawing.Point(132, 12);
+            this.note1ComboBox.Location = new System.Drawing.Point(176, 15);
+            this.note1ComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.note1ComboBox.Name = "note1ComboBox";
-            this.note1ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.note1ComboBox.Size = new System.Drawing.Size(160, 24);
             this.note1ComboBox.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Location = new System.Drawing.Point(15, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.Size = new System.Drawing.Size(54, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Note 1:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 62);
+            this.label2.Location = new System.Drawing.Point(15, 76);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.Size = new System.Drawing.Size(54, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Note 2:";
             // 
             // note2ComboBox
             // 
             this.note2ComboBox.FormattingEnabled = true;
-            this.note2ComboBox.Location = new System.Drawing.Point(132, 59);
+            this.note2ComboBox.Location = new System.Drawing.Point(176, 73);
+            this.note2ComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.note2ComboBox.Name = "note2ComboBox";
-            this.note2ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.note2ComboBox.Size = new System.Drawing.Size(160, 24);
             this.note2ComboBox.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 111);
+            this.label3.Location = new System.Drawing.Point(15, 137);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.Size = new System.Drawing.Size(54, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Note 3:";
             // 
             // note3ComboBox
             // 
             this.note3ComboBox.FormattingEnabled = true;
-            this.note3ComboBox.Location = new System.Drawing.Point(132, 108);
+            this.note3ComboBox.Location = new System.Drawing.Point(176, 133);
+            this.note3ComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.note3ComboBox.Name = "note3ComboBox";
-            this.note3ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.note3ComboBox.Size = new System.Drawing.Size(160, 24);
             this.note3ComboBox.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 158);
+            this.label4.Location = new System.Drawing.Point(15, 194);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.Size = new System.Drawing.Size(54, 17);
             this.label4.TabIndex = 7;
             this.label4.Text = "Note 4:";
             // 
             // note4ComboBox
             // 
             this.note4ComboBox.FormattingEnabled = true;
-            this.note4ComboBox.Location = new System.Drawing.Point(132, 155);
+            this.note4ComboBox.Location = new System.Drawing.Point(176, 191);
+            this.note4ComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.note4ComboBox.Name = "note4ComboBox";
-            this.note4ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.note4ComboBox.Size = new System.Drawing.Size(160, 24);
             this.note4ComboBox.TabIndex = 6;
             // 
             // backButton
             // 
-            this.backButton.Location = new System.Drawing.Point(12, 196);
+            this.backButton.Location = new System.Drawing.Point(16, 241);
+            this.backButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(79, 33);
+            this.backButton.Size = new System.Drawing.Size(105, 41);
             this.backButton.TabIndex = 8;
             this.backButton.Text = "Back";
             this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(183, 196);
+            this.submitButton.Location = new System.Drawing.Point(244, 241);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(79, 33);
+            this.submitButton.Size = new System.Drawing.Size(105, 41);
             this.submitButton.TabIndex = 9;
             this.submitButton.Text = "Submit";
             this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
             // 
             // strikeButton
             // 
-            this.strikeButton.Location = new System.Drawing.Point(97, 196);
+            this.strikeButton.Location = new System.Drawing.Point(129, 241);
+            this.strikeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.strikeButton.Name = "strikeButton";
-            this.strikeButton.Size = new System.Drawing.Size(79, 33);
+            this.strikeButton.Size = new System.Drawing.Size(105, 41);
             this.strikeButton.TabIndex = 10;
             this.strikeButton.Text = "Strike";
             this.strikeButton.UseVisualStyleBackColor = true;
+            this.strikeButton.Click += new System.EventHandler(this.strikeButton_Click);
             // 
             // ChordQualitiesForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 237);
+            this.ClientSize = new System.Drawing.Size(365, 292);
             this.Controls.Add(this.strikeButton);
             this.Controls.Add(this.submitButton);
             this.Controls.Add(this.backButton);
@@ -153,8 +167,9 @@ namespace KTANE_Solver
             this.Controls.Add(this.note2ComboBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.note1ComboBox);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "ChordQualitiesForm";
-            this.Text = "ChordQualitiesForm";
+            this.Text = "KTANE Bot by Hawker";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ChordQualitiesForm.cs
+++ ChordQualitiesForm.cs
@@ -7,14 +7,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace KTANE_Solver
 {
-    public partial class ChordQualitiesForm : Form
+    public partial class ChordQualitiesForm : ModuleForm
     {
-        public ChordQualitiesForm()
+        public ChordQualitiesForm(Bomb bomb, StreamWriter logFileWriter, ModuleSelectionForm moduleSelectionForm) : base(bomb, logFileWriter, moduleSelectionForm, "Chord Qualities", false)
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            UpdateForm();
+        }
+
+        private void UpdateForm()
+        {
+            UpdateNoteComboBox(note1ComboBox);
+            UpdateNoteComboBox(note2ComboBox);
+            UpdateNoteComboBox(note3ComboBox);
+            UpdateNoteComboBox(note4ComboBox);
+
+        }
+
+        private void UpdateNoteComboBox(ComboBox comboBox)
+        {
+            string[] notes = new string[] { "A", "ASharp", "B", "C", "CSharp", "D", "DSharp", "E", "F", "FSharp", "G", "GSharp"};
+
+            comboBox.Items.Clear();
+            comboBox.Items.AddRange(notes);
+            comboBox.Text = notes[0];
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            GoToMoudleSelectionForm();
+        }
+
+        private void strikeButton_Click(object sender, EventArgs e)
+        {
+            IncrementStrike();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            ChordQualities.Note note1 = ConvertStringToNote(note1ComboBox.Text);
+            ChordQualities.Note note2 = ConvertStringToNote(note2ComboBox.Text);
+            ChordQualities.Note note3 = ConvertStringToNote(note3ComboBox.Text);
+            ChordQualities.Note note4 = ConvertStringToNote(note4ComboBox.Text);
+
+            if (note1 == note2 || note1 == note3 || note1 == note4 ||
+                                 note2 == note3 || note2 == note4 ||
+                                                   note3 == note4)
+            {
+                ShowErrorMessage("Can't have duplicate Notes");
+                return;
+            }
+
+            ChordQualities module = new ChordQualities(Bomb, LogFileWriter, note1, note2, note3, note4);
+            module.Solve();
+            UpdateForm();
+        }
+
+        private ChordQualities.Note ConvertStringToNote(string note)
+        {
+            switch (note)
+            {
+                case "A":
+                    return ChordQualities.Note.A;
+
+                case "ASharp":
+                    return ChordQualities.Note.ASharp;
+
+                case "B":
+                    return ChordQualities.Note.B;
+
+                case "C":
+                    return ChordQualities.Note.C;
+
+                case "CSharp":
+                    return ChordQualities.Note.CSharp;
+
+                case "D":
+                    return ChordQualities.Note.D;
+
+                case "DSharp":
+                    return ChordQualities.Note.DSharp;
+
+                case "E":
+                    return ChordQualities.Note.E;
+
+                case "F":
+                    return ChordQualities.Note.F;
+
+                case "FSharp":
+                    return ChordQualities.Note.FSharp;
+
+                case "G":
+                    return ChordQualities.Note.G;
+
+                default:
+                    return ChordQualities.Note.GSharp;
+            }
         }
     }
 }
