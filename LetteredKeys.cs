c191d4e12200519132f6b52971042f15ee4eda32
Nya <nyabentley@gmail.com>
2022-08-08 19:47:55 -0400

    Created Lettered Keys Solver

---
+++ LetteredKeys.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KTANE_Solver
+{
+    public class LetteredKeys : Module
+    {
+        int number;
+        public LetteredKeys(Bomb bomb, StreamWriter logFileWriter, int number) : base(bomb, logFileWriter, "Lettered Keys")
+        {
+            this.number = number;
+        }
+
+        public string SolveDebug()
+        {
+            if (number == 69)
+                return "D";
+
+            if (number % 6 == 0)
+                return "A";
+
+            if (Bomb.Battery >= 2 && number % 3 == 0)
+                return "B";
+
+            bool containsCE3 = Bomb.SerialNumber.Contains('C') || Bomb.SerialNumber.Contains('E') || Bomb.SerialNumber.Contains('3');
+
+            if (containsCE3 && number >= 22 && number <= 79)
+                return "B";
+
+            if (containsCE3)
+                return "C";
+
+            if (number < 46)
+                return "D";
+
+            return "A";
+        }
+
+        public void Solve()
+        {
+            ShowAnswer(SolveDebug(), true);
+        }
+    }
+}
---
+++ LetteredKeysForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace KTANE_Solver
+{
+    partial class LetteredKeysForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.stikeButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "What is the number?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 71);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(80, 30);
+            this.backButton.TabIndex = 2;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // stikeButton
+            // 
+            this.stikeButton.Location = new System.Drawing.Point(115, 71);
+            this.stikeButton.Name = "stikeButton";
+            this.stikeButton.Size = new System.Drawing.Size(80, 30);
+            this.stikeButton.TabIndex = 3;
+            this.stikeButton.Text = "Strike";
+            this.stikeButton.UseVisualStyleBackColor = true;
+            this.stikeButton.Click += new System.EventHandler(this.stikeButton_Click);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(211, 71);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(80, 30);
+            this.submitButton.TabIndex = 4;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // LetteredKeysForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 113);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.stikeButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "LetteredKeysForm";
+            this.Text = "LetteredKeysForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button stikeButton;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
