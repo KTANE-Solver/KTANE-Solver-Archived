f8386bd648f2ed24dd583cb1f83b2c7d77df62a6
Nya <nyabentley@gmail.com>
2022-08-10 18:32:23 -0400

    Created Caesar Cipher solver

---
+++ CaesarChipherForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace KTANE_Solver
+{
+    partial class CaesarChipherForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.strikeButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Display:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 106);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(88, 36);
+            this.backButton.TabIndex = 2;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // strikeButton
+            // 
+            this.strikeButton.Location = new System.Drawing.Point(106, 106);
+            this.strikeButton.Name = "strikeButton";
+            this.strikeButton.Size = new System.Drawing.Size(88, 36);
+            this.strikeButton.TabIndex = 3;
+            this.strikeButton.Text = "Strike";
+            this.strikeButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(200, 106);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(88, 36);
+            this.submitButton.TabIndex = 4;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // CaesarChipherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 151);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.strikeButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "CaesarChipherForm";
+            this.Text = "CaesarChipherForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button strikeButton;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
---
+++ CaesarCipher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace KTANE_Solver
+{
+    class CaesarCipher : Module
+    {
+        string display;
+        int offset;
+
+        public CaesarCipher(string display, Bomb bomb, StreamWriter logFileWriter) : base(bomb, logFileWriter, "Caesar Cipher")
+        {
+            this.display = display;
+            offset = 0;
+        }
+
+        public void Solve()
+        {
+            FindOffset();
+
+            string newDisplay = "";
+            for (int i = 0; i < display.Length; i++)
+            {
+                newDisplay +=  "" + (display[i] + offset);
+            }
+
+            ShowAnswer(newDisplay, true);
+        }
+
+        private void FindOffset()
+        {
+            if (Bomb.Parallel.Visible && Bomb.Nsa.Lit)
+            {
+                return;
+            }
+
+            if (Bomb.HasVowel)
+            {
+                offset -= -1;
+            }
+
+            offset += Bomb.Battery;
+
+            if (Bomb.LastDigit % 2 == 0)
+            {
+                offset += 1;
+            }
+
+            if (Bomb.Car.Visible)
+            {
+                offset += 1;
+            }
+        }
+    }
+}
