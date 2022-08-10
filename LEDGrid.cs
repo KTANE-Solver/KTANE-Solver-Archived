ee8fc2ca46d409431fcf0f28434b7e48053db404
Nya <nyabentley@gmail.com>
2022-08-10 17:56:26 -0400

    Created LED Grid Solver

---
+++ LEDGrid.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace KTANE_Solver
+{
+    class LEDGrid : Module
+    {
+        Color[] grid;
+
+        public LEDGrid(Bomb bomb, StreamWriter logFileWriter, Color[] grid) : base(bomb, logFileWriter, "LED Grid")
+        {
+            this.grid = grid;
+        }
+
+        public void Solve()
+        {
+            ShowAnswer(FindAnswer(), true);
+        }
+
+        public string FindAnswer()
+        {
+            switch (Bomb.UnlitIndicatorsList.Count)
+            {
+                case 0:
+                    if (grid.Where(x => x == Color.Orange).Count() == 0)
+                    {
+                        return "CDAB";
+                    }
+
+                    if (grid.Where(x => x == Color.Red).Count() >= 3)
+                    {
+                        return "DACB";
+                    }
+
+                    if (TwoPairs())
+                    {
+                        return "BACD";
+                    }
+
+                    if (RowSameColor(2))
+                    {
+                        return "ACDB";
+                    }
+
+                    return "BCDA";
+
+                case 1:
+
+                    if (grid.Distinct().ToList().Count == grid.Length)
+                    {
+                        return "DCBA";
+                    }
+
+                    if (RowSameColor(0))
+                    {
+                        return "DACB";
+                    }
+
+                    if (grid.Where(x => x == Color.Red).Count() == 3 || grid.Where(x => x == Color.Pink).Count() == 3 || grid.Where(x => x == Color.Purple).Count() == 3)
+                    {
+                        return "BACD";
+                    }
+
+                    if (grid.Where(x => x == Color.White).Count() == 1 || grid.Where(x => x == Color.Blue).Count() == 2 || grid.Where(x => x == Color.Yellow).Count() == 3)
+                    {
+                        return "ACDB";
+                    }
+
+                    return "DBAC";
+
+                case 2:
+
+                    if (grid.Where(x => x == Color.Red).Count() >= 3)
+                    {
+                        return "DCBA";
+                    }
+
+                    if (TwoPairs())
+                    {
+                        return "DACB";
+                    }
+
+                    if (grid.Where(x => x == Color.White).Count() > 1 || grid.Where(x => x == Color.Orange).Count() > 1 || grid.Where(x => x == Color.Pink).Count() > 1)
+                    {
+                        return "BACD";
+                    }
+
+                    if (grid.Where(x => x == Color.Green).Count() == 1 || grid.Where(x => x == Color.Yellow).Count() == 2 || grid.Where(x => x == Color.Red).Count() == 3 || grid.Where(x => x == Color.Blue).Count() == 4)
+                    {
+                        return "ACDB";
+                    }
+
+                    return "DBAC";
+
+                case 3:
+
+                    if (grid.Where(x => x == Color.Orange).Count() == 2)
+                    {
+                        return "DCBA";
+                    }
+
+                    if (PairCount() > 1)
+                    {
+                        return "DACB";
+                    }
+
+                    if (grid.Where(x => x == Color.Purple).Count() == 0)
+                    {
+                        return "BACD";
+                    }
+
+                    if (grid.Where(x => x == Color.Red).Count() == 1 && grid.Where(x => x == Color.Yellow).Count() == 1)
+                    {
+                        return "ACDB";
+                    }
+
+                    return "DBAC";
+
+                default:
+
+                    if (RowSameColor(1))
+                    {
+                        return "DCBA";
+                    }
+
+                    if (grid.Where(x => x == Color.Green).Count() >= 2)
+                    {
+                        return "DACB";
+                    }
+
+                    if (TwoPairs())
+                    {
+                        return "BACD";
+                    }
+
+                    if (grid.Where(x => x == Color.Pink).Count() == 0)
+                    {
+                        return "ACDB";
+                    }
+
+                    return "DBAC";
+            }
+        }
+
+        private bool RowSameColor(int row)
+        {
+            Color color = grid[row * 3];
+
+            for (int i = 1; i < 3; i++)
+            {
+                if (color != grid[row * 3 + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool TwoPairs()
+        {
+            return PairCount() == 2;
+        }
+
+        private int PairCount()
+        {
+           return grid.Length - grid.Distinct().ToList().Count;
+        }
+    }
+}
---
+++ LEDGridForm.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace KTANE_Solver
+{
+    partial class LEDGridForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox8 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox9 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.backButton = new System.Windows.Forms.Button();
+            this.strikeButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Color 1:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(172, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(172, 40);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Color 1:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(172, 67);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Color 1:";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(172, 94);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Color 1:";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(172, 121);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Color 1:";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(172, 148);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Color 1:";
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Location = new System.Drawing.Point(172, 175);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(121, 21);
+            this.comboBox7.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Color 1:";
+            // 
+            // comboBox8
+            // 
+            this.comboBox8.FormattingEnabled = true;
+            this.comboBox8.Location = new System.Drawing.Point(172, 202);
+            this.comboBox8.Name = "comboBox8";
+            this.comboBox8.Size = new System.Drawing.Size(121, 21);
+            this.comboBox8.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 205);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Color 1:";
+            // 
+            // comboBox9
+            // 
+            this.comboBox9.FormattingEnabled = true;
+            this.comboBox9.Location = new System.Drawing.Point(172, 229);
+            this.comboBox9.Name = "comboBox9";
+            this.comboBox9.Size = new System.Drawing.Size(121, 21);
+            this.comboBox9.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 232);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Color 1:";
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 282);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(90, 35);
+            this.backButton.TabIndex = 18;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // strikeButton
+            // 
+            this.strikeButton.Location = new System.Drawing.Point(108, 282);
+            this.strikeButton.Name = "strikeButton";
+            this.strikeButton.Size = new System.Drawing.Size(90, 35);
+            this.strikeButton.TabIndex = 19;
+            this.strikeButton.Text = "Strike";
+            this.strikeButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(204, 282);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(90, 35);
+            this.submitButton.TabIndex = 20;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // LEDGridForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 325);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.strikeButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.comboBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "LEDGridForm";
+            this.Text = "LEDGridForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button strikeButton;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
