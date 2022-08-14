20716fe7b961ebee45ab3caf7a0f10f7b21f028a
Nya <nyabentley@gmail.com>
2022-08-14 16:58:23 -0400

    Started working on LED Grid tests

--- LEDGrid.cs
+++ LEDGrid.cs
@@ -8,7 +8,7 @@ using System.Drawing;
 
 namespace KTANE_Solver
 {
-    class LEDGrid : Module
+    public class LEDGrid : Module
     {
         Color[] grid;
 
@@ -24,125 +24,152 @@ namespace KTANE_Solver
 
         public string FindAnswer()
         {
+            PrintDebugLine("Number of unlit indicators: " + Bomb.UnlitIndicatorsList.Count);
+
             switch (Bomb.UnlitIndicatorsList.Count)
             {
                 case 0:
                     if (grid.Where(x => x == Color.Orange).Count() == 0)
                     {
+                        PrintDebugLine("There are no orange LEDs");
                         return "CDAB";
                     }
 
                     if (grid.Where(x => x == Color.Red).Count() >= 3)
                     {
+                        PrintDebugLine("There are three or more red LEDs");
                         return "DACB";
                     }
 
-                    if (TwoPairs())
+                    if (PairCount() >= 2)
                     {
+                        PrintDebugLine("There are two or more pairs");
                         return "BACD";
                     }
 
                     if (RowSameColor(2))
                     {
+                        PrintDebugLine("The bottom row is all the same colour");
                         return "ACDB";
                     }
 
+                    PrintDebugLine("None of the other conditions applied");
                     return "BCDA";
 
                 case 1:
 
                     if (grid.Distinct().ToList().Count == grid.Length)
                     {
+                        PrintDebugLine("Every LED is unique");
                         return "DCBA";
                     }
 
                     if (RowSameColor(0))
                     {
+                        PrintDebugLine("The top row is all the same colour");
                         return "DACB";
                     }
 
                     if (grid.Where(x => x == Color.Red).Count() == 3 || grid.Where(x => x == Color.Pink).Count() == 3 || grid.Where(x => x == Color.Purple).Count() == 3)
                     {
+                        PrintDebugLine("There are exactly three red, three pink or three purple LEDs");
                         return "BACD";
                     }
 
                     if (grid.Where(x => x == Color.White).Count() == 1 || grid.Where(x => x == Color.Blue).Count() == 2 || grid.Where(x => x == Color.Yellow).Count() == 3)
                     {
+                        PrintDebugLine("There is exactly one white, two blue or three yellow LEDs");
                         return "ACDB";
                     }
 
+                    PrintDebugLine("None of the other conditions applied");
                     return "DBAC";
 
                 case 2:
 
                     if (grid.Where(x => x == Color.Red).Count() >= 3)
                     {
+                        PrintDebugLine("There are three or more purple LEDs");
                         return "DCBA";
                     }
 
                     if (TwoPairs())
                     {
+                        PrintDebugLine("There are exactly two pairs");
                         return "DACB";
                     }
 
                     if (grid.Where(x => x == Color.White).Count() > 1 || grid.Where(x => x == Color.Orange).Count() > 1 || grid.Where(x => x == Color.Pink).Count() > 1)
                     {
+                        PrintDebugLine("There is at least one white, one orange and one pink LED");
                         return "BACD";
                     }
 
                     if (grid.Where(x => x == Color.Green).Count() == 1 || grid.Where(x => x == Color.Yellow).Count() == 2 || grid.Where(x => x == Color.Red).Count() == 3 || grid.Where(x => x == Color.Blue).Count() == 4)
                     {
+                        PrintDebugLine("There is exactly one green, two yellow, three red or four blue LEDs");
                         return "ACDB";
                     }
 
+                    PrintDebugLine("None of the other conditions applied");
                     return "DBAC";
 
                 case 3:
 
                     if (grid.Where(x => x == Color.Orange).Count() == 2)
                     {
+                        PrintDebugLine("There are exactly two orange LEDs");
                         return "DCBA";
                     }
 
                     if (PairCount() > 1)
                     {
+                        PrintDebugLine("There is more than one pair");
                         return "DACB";
                     }
 
                     if (grid.Where(x => x == Color.Purple).Count() == 0)
                     {
+                        PrintDebugLine("There are no purple LEDs");
                         return "BACD";
                     }
 
                     if (grid.Where(x => x == Color.Red).Count() == 1 && grid.Where(x => x == Color.Yellow).Count() == 1)
                     {
+                        PrintDebugLine("There is at least one red and one yellow LED");
                         return "ACDB";
                     }
 
+                    PrintDebugLine("None of the other conditions applied");
                     return "DBAC";
 
                 default:
 
                     if (RowSameColor(1))
                     {
+                        PrintDebugLine("The centre row is all the same colour");
                         return "DCBA";
                     }
 
                     if (grid.Where(x => x == Color.Green).Count() >= 2)
                     {
+                        PrintDebugLine("There are two or more green LEDs");
                         return "DACB";
                     }
 
                     if (TwoPairs())
                     {
+                        PrintDebugLine("There are exactly two pairs");
                         return "BACD";
                     }
 
                     if (grid.Where(x => x == Color.Pink).Count() == 0)
                     {
+                        PrintDebugLine("There are no pink LEDs");
                         return "ACDB";
                     }
 
+                    PrintDebugLine("None of the other conditions applied");
                     return "DBAC";
             }
         }
---
+++ ModuleTests/LEDGridTest.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using KTANE_Solver;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ModuleTests
+{
+    [TestClass]
+    public class LEDGridTest
+    {
+        StreamWriter streamWriter = new StreamWriter("dummy.txt");
+
+        [TestMethod]
+        public void ZeroIndicators()
+        {
+            Bomb bomb = new Bomb(Day.Sunday, "ABCDE6", 0, 0, 
+                                 new Indicator("", true, false), new Indicator("", false, false), 
+                                 new Indicator("", false, false), new Indicator("", false, false), 
+                                 new Indicator("", false, false), new Indicator("", false, false), 
+                                 new Indicator("", false, false), new Indicator("", false, false), 
+                                 new Indicator("", false, false), new Indicator("", false, false), 
+                                 new Indicator("", false, false), false, 0, 
+                                 new Port("", 0), new Port("", 0), new Port("", 0), 
+                                 new Port("", 0), new Port("", 0), new Port("", 0));
+
+            Color[] grid = new Color[] { Color.Red, Color.Red, Color.Red, 
+                                         Color.Red, Color.Red, Color.Red, 
+                                         Color.Red, Color.Red, Color.Red };
+
+            LEDGrid module = new LEDGrid(bomb, streamWriter, grid);
+
+            Assert.AreEqual("C, D, A, B", module.FindAnswer());
+
+            grid = new Color[] { Color.Red, Color.Red, Color.Red,
+                                 Color.Red, Color.Red, Color.Red,
+                                 Color.Red, Color.Red, Color.Orange };
+
+            module = new LEDGrid(bomb, streamWriter, grid);
+
+            Assert.AreEqual("D, A, C, B", module.FindAnswer());
+
+            grid = new Color[] { Color.Blue, Color.Blue, Color.Blue,
+                                 Color.Blue, Color.Blue, Color.Orange,
+                                 Color.Blue, Color.Blue, Color.Orange };
+
+            module = new LEDGrid(bomb, streamWriter, grid);
+
+            Assert.AreEqual("B, A, C, D", module.FindAnswer());
+
+            grid = new Color[] { Color.Orange, Color.Blue, Color.Blue,
+                                 Color.Blue, Color.Blue, Color.Blue,
+                                 Color.Blue, Color.Blue, Color.Blue };
+
+            module = new LEDGrid(bomb, streamWriter, grid);
+
+            Assert.AreEqual("A, C, D, B", module.FindAnswer());
+
+            grid = new Color[] { Color.Orange, Color.Blue, Color.Blue,
+                                 Color.Blue, Color.Blue, Color.Blue,
+                                 Color.Blue, Color.Green, Color.Blue };
+
+            module = new LEDGrid(bomb, streamWriter, grid);
+
+            Assert.AreEqual("B, C, D, A", module.FindAnswer());
+        }
+
+        [TestMethod]
+        public void OneIndicators()
+        {
+
+        }
+
+        [TestMethod]
+        public void TwoIndicators()
+        {
+
+        }
+
+        [TestMethod]
+        public void ThreeIndicators()
+        {
+
+        }
+
+        [TestMethod]
+        public void FourIndicators()
+        {
+
+        }
+    }
+}
