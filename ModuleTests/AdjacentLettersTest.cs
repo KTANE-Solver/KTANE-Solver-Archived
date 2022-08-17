999c08c20806398a8f8e3de087f9939f1d48711f
Nya <nyabentley@gmail.com>
2022-08-17 15:03:47 -0400

    Got Adjacent Letters tests log

---
+++ ModuleTests/AdjacentLettersTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ModuleTests
+{
+    [TestClass]
+    public class AdjacentLettersTest
+    {
+        //https://ktane.timwi.de/More/Logfile%20Analyzer.html#file=5f21cd7b177e661ac0f99e81c7e4e20e6d14d6af;bomb=HK7VW3
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
