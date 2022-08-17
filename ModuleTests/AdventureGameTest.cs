82e067534f485e904b5608bac054bbcc16ac2d32
Nya <nyabentley@gmail.com>
2022-08-17 15:09:32 -0400

    Added adventure game log

---
+++ ModuleTests/AdventureGameTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ModuleTests
+{
+    //https://ktane.timwi.de/More/Logfile%20Analyzer.html#file=eda837983b74521e70e2aeb677a25b0d3cc48b21
+    [TestClass]
+    public class AdventureGameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
